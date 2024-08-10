4b04bd90a7d106d5056b2fe761fb637b8d60424a
Shelton Lázio Agostinho <lazioshelton@gmail.com>
2024-08-10 17:19:39 +0100

    Adding the crew members

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 // To generate random number between 0 and range (not included)
 const randomNumber = (range) => Math.floor(Math.random() * range);
 
-
+const luffysCrew = [
+    "Monkey D. Luffy",
+    "Roronoa Zoro",
+    "Nami",
+    "Usopp",
+    "Sanji",
+    "Tony Tony Chopper",
+    "Nico Robin",
+    "Franky",
+    "Brook",
+    "Jinbe"
+];
